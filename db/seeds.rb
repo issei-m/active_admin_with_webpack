a0d3627af192b96d4d2d76fb0d99e3fa46ca3d89
Issei.M <issei.m7@gmail.com>
2023-08-02 16:56:22 +0900

    Enhance setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.where(email: 'admin@example.com').first_or_create { |au| au.password = 'password'; au.password_confirmation = 'password' } if Rails.env.development?
